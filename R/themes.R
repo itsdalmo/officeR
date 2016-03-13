d5ad8ee0a9bb121484ccdaa84aa735c28c3c49b9
dalmo@osx <kristian@doingit.no>
2016-03-14 00:55:17 +0100

    General cleanup of to_excel. TODO: Fix openxlsx styling.

--- R/themes.R
+++ R/themes.R
@@ -1,66 +1,58 @@
 # Function used to format style and values in xlsx wrappers --------------------
 
-format_xlsx <- function(df, wb, sheet, table_row, style = TRUE, values = TRUE) {
+format_xlsx <- function(wb, sheet, types, row, dims) {
 
-  # Get row and column index
-  title_row <- table_row - 1
-  last_row <- table_row + nrow(df)
-  all_rows <- table_row:last_row
-  all_cols <- 1:ncol(df)
+  # Style the title and merge columns
+  rows <- row - 1L; cols <- 1:dims[1]
+  openxlsx::addStyle(wb, sheet, xlsx_style_title, rows = rows, cols = cols, gridExpand = TRUE)
+  openxlsx::mergeCells(wb, sheet, rows = rep(rows, length(cols)), cols = cols)
 
-  # Optional styling of the table
-  if (isTRUE(style)) {
-    # Add title, style the cells and merge them
-    openxlsx::addStyle(wb, sheet, xlsx_style_title, rows = title_row, cols = all_cols, gridExpand = TRUE)
-    openxlsx::mergeCells(wb, sheet, rows = rep(title_row, length(all_cols)), cols = all_cols)
+  # Apply baseline style to the data
+  rows <- row:(row + dims[2]); cols <- 1:dims[1]
+  openxlsx::addStyle(wb, sheet, xlsx_style_table, rows = rows, cols = cols, gridExpand = TRUE)
 
-    # Style the entire table
-    openxlsx::addStyle(wb, sheet, xlsx_style_table, rows = all_rows, cols = all_cols, gridExpand = TRUE)
+  # Change styling for first (header)
+  rows <- row; cols <- 1:dims[1]
+  openxlsx::addStyle(wb, sheet, xlsx_style_firstrow, rows = rows, cols = cols, gridExpand = TRUE)
 
-    # Alter the style of first row
-    openxlsx::addStyle(wb, sheet, xlsx_style_firstrow, rows = table_row, cols = all_cols, gridExpand = TRUE)
+  # ..  and last row
+  rows <- row + dims[2]; cols <- 1:dims[1]
+  openxlsx::addStyle(wb, sheet, xlsx_style_lastrow, rows = rows, cols = cols, gridExpand = TRUE)
 
-    # ..  and last row
-    openxlsx::addStyle(wb, sheet, xlsx_style_lastrow, rows = last_row, cols = all_cols, gridExpand = TRUE)
+
+  # Get column indicies
+  fct_columns <- which(vapply(df, is.factor, logical(1)))
+  chr_columns <- which(vapply(df, is.character, logical(1)))
+  int_columns <- which(vapply(df, is.integer, logical(1)))
+  pct_columns <- which(vapply(df, function(x) all(is.numeric(x) && x <= 1 && x >= 0), logical(1)))
+  num_columns <- which(vapply(df, is.numeric, logical(1)))
+
+  num_columns <- setdiff(num_columns, c(int_columns, pct_columns))
+
+  # Format numeric to 1 decimal place
+  if (length(num_columns)) {
+    openxlsx::addStyle(wb, sheet, xlsx_numeric, rows = all_rows, cols = num_columns,
+                       gridExpand = TRUE, stack = TRUE)
   }
 
-  # Optional formatting for numeric values
-  if (isTRUE(values)) {
-
-    # Get column indicies
-    fct_columns <- which(vapply(df, is.factor, logical(1)))
-    chr_columns <- which(vapply(df, is.character, logical(1)))
-    int_columns <- which(vapply(df, is.integer, logical(1)))
-    pct_columns <- which(vapply(df, function(x) all(is.numeric(x) && x <= 1 && x >= 0), logical(1)))
-    num_columns <- which(vapply(df, is.numeric, logical(1)))
-
-    num_columns <- setdiff(num_columns, c(int_columns, pct_columns))
-
-    # Format numeric to 1 decimal place
-    if (length(num_columns)) {
-      openxlsx::addStyle(wb, sheet, xlsx_numeric, rows = all_rows, cols = num_columns,
-                         gridExpand = TRUE, stack = TRUE)
-    }
-
-    # Format integer to 0 decimal places
-    if (length(int_columns)) {
-      openxlsx::addStyle(wb, sheet, xlsx_integer, rows = all_rows, cols = int_columns,
-                         gridExpand = TRUE, stack = TRUE)
-    }
-
-    # Format percentages
-    if (length(pct_columns)) {
-      openxlsx::addStyle(wb, sheet, xlsx_percent, rows = all_rows, cols = pct_columns,
-                         gridExpand = TRUE, stack = TRUE)
-    }
-
-    # Change the text orientation of character columns
-    if (length(chr_columns) || length(fct_columns)) {
-      openxlsx::addStyle(wb, sheet, xlsx_character, rows = all_rows, cols = c(chr_columns, fct_columns),
-                         gridExpand = TRUE, stack = TRUE)
-    }
-
+  # Format integer to 0 decimal places
+  if (length(int_columns)) {
+    openxlsx::addStyle(wb, sheet, xlsx_integer, rows = all_rows, cols = int_columns,
+                       gridExpand = TRUE, stack = TRUE)
   }
+
+  # Format percentages
+  if (length(pct_columns)) {
+    openxlsx::addStyle(wb, sheet, xlsx_percent, rows = all_rows, cols = pct_columns,
+                       gridExpand = TRUE, stack = TRUE)
+  }
+
+  # Change the text orientation of character columns
+  if (length(chr_columns) || length(fct_columns)) {
+    openxlsx::addStyle(wb, sheet, xlsx_character, rows = all_rows, cols = c(chr_columns, fct_columns),
+                       gridExpand = TRUE, stack = TRUE)
+  }
+
 
 }
 
@@ -104,18 +96,6 @@ xlsx_style_firstrow <- openxlsx::createStyle(
   border = "Bottom",
   borderColour = "#0094A5",
   borderStyle = "thin",
-  fgFill = "#CCE9EB",
-  halign = "center",
-  valign = "center"
-)
-
-xlsx_style_lastrow <- openxlsx::createStyle(
-  fontName = "Trebuchet MS",
-  fontSize = 8,
-  fontColour = "#000000",
-  border = "Bottom",
-  borderColour = "#0094A5",
-  borderStyle = "medium",
   fgFill = "#CCE9EB",
   halign = "center",
   valign = "center"
--- R/to_excel.R
+++ R/to_excel.R
@@ -23,63 +23,56 @@
 #' x %>% to_sheet(wb, sheet = "test", append = FALSE)
 #' openxlsx::saveWorkbook(wb, "test.xlsx", overwrite = TRUE)
 
-to_excel <- function(df, wb = NULL, title = "Table", sheet = "tables", row = 1L,
-                     format_style = TRUE, format_values = TRUE, append = TRUE) {
+to_excel <- function(df, wb, title = " ", format = !is.null(title),
+                     sheet = "tables", row = 1L, append = TRUE) {
 
-  if (is.null(wb)) {
-    wb <- Workbook$new()
+  if (!is.string(sheet)) {
+    stop("The sheet has to be a string (character(1)).")
+  } else if (!is.null(title) && !is.string(title)) {
+    stop("Title has to be either NULL or a string.")
   } else if (!inherits(wb, "Workbook")) {
     stop("wb argument must be a (loaded) openxlsx workbook")
   }
 
-  if (!is.string(sheet)) {
-    stop("The sheet has to be a string (character(1)).", call. = FALSE)
-  }
-
-  sheet_exists <- sheet %in% openxlsx::sheets(wb)
-
   # Get last row if sheet exists, or create if it does not.
-  if (sheet_exists && isTRUE(append)) {
-
-    row <- 2L + nrow(openxlsx::read.xlsx(wb, sheet = sheet, colNames = FALSE, skipEmptyRows = FALSE))
-  } else if (sheet_exists) {
-    openxlsx::removeWorksheet(wb, sheet)
-    openxlsx::addWorksheet(wb, sheetName = sheet)
-  } else {
-    openxlsx::addWorksheet(wb, sheetName = sheet)
-  }
-
-  # Set table_row to be the last found row
-  table_row <- row
-
-  # Add data to the workbook
-  if (is.null(names(df)) || identical(names(df), character(0))) {
-    warning(sheet, ": No columnames in data. An empty sheet was created", call. = FALSE)
-  } else {
-
-    # When styling the title must be written first (and convert df names to titles)
-    if (isTRUE(format_style)) {
-      openxlsx::writeData(wb, sheet, title, startRow = row)
-      names(df) <- stri_trans_totitle(names(df), type = "sentence")
-      table_row <- row + 1
+  if (sheet %in% openxlsx::sheets(wb)) {
+    if (append) {
+      row <- nrow(openxlsx::read.xlsx(wb, sheet = sheet, colNames = FALSE, skipEmptyRows = FALSE))
+      row <- row + 2L # Add spacing between tables.
+    } else {
+      openxlsx::removeWorksheet(wb, sheet)
+      openxlsx::addWorksheet(wb, sheetName = sheet)
     }
-
-    # Write the data.frame
-    openxlsx::writeData(wb, sheet, df, startRow = table_row)
+  } else {
+    openxlsx::addWorksheet(wb, sheetName = sheet)
   }
+
+  # Return early if there are no colummnames in the data.
+  if (is.null(names(df)) || identical(names(df), character(0))) {
+    warning("No columnames in data. An empty sheet was created: ", paste0("'", sheet, "'"))
+    return()
+  }
+
+  # Write title and titlecase colnames when formatting.
+  if (format) {
+    openxlsx::writeData(wb, sheet, title, startRow = row)
+    substr(names(df), 1L, 1L) <- toupper(substr(names(df), 1L, 1L))
+    row <- row + 1L # Title has been written to the first row.
+  }
+
+  # Write the rest of the data before formatting.
+  openxlsx::writeData(wb, sheet, df, startRow = row)
 
   # Apply additional formatting if desired
-  if (isTRUE(format_style) || isTRUE(format_values)) {
-
-    format_xlsx(df, wb, sheet, table_row, style = format_style, values = format_values)
-
+  if (format) {
+    coltypes <- vapply(df, function(x) class(x)[1], character(1))
+    format_xlsx(wb, sheet, coltypes, row, dim(df))
   }
 
   # Check the dimensions of what was written
-  n_row <- dim(df)
-  n_row <- if(is.null(n_row)) 0 else n_row[1]
+  last_row <- nrow(df) %||% 0L
 
-  # Invisibly return the rows that have been written to
-  invisible(setNames(c(table_row, table_row + n_row), c("first", "last")))
+  # TODO: Why did I want this again?
+  invisible(setNames(c(row, row + last_row), c("first", "last")))
 
 }
