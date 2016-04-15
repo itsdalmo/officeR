1a4532723a25654e2dae01d328be59f9352fe867
dalmo@pwin <kristian@doingit.no>
2016-04-15 17:59:31 +0200

    Tests for sharepoint links/mounts.

---
+++ tests/testthat/test-sharepoint.R
@@ -0,0 +1,30 @@
+context("sharepoint")
+
+lnk <- "http://www.such-test.com/directory/file.txt"
+
+test_that("sharepoint_link returns correct class" , {
+  skip_if_not_installed("httr")
+  sp <- sharepoint_link(lnk)
+
+  expect_error(sharepoint_link(c("a", "b")), "must be a string")
+  expect_s3_class(sp, "sharepoint")
+
+})
+
+test_that("We can call read_data on sharepoint links" , {
+  # Slow test due to timeout.
+  skip_if_not_installed("httr")
+  sp <- sharepoint_link(lnk)
+
+  # Set user/password to test
+  httr:::set_envvar("sp_usr", "test", "session")
+  httr:::set_envvar("sp_pwd", "test", "session")
+
+  expect_error(read_data(sp), "Couldn't resolve")
+})
+
+test_that("read_data on sharepoint mounts" , {
+  skip_if_not(on_windows())
+  sp <- sharepoint_link(lnk, mounted = TRUE)
+  expect_error(read_data(sp), "Path does not exist")
+})
