abce7cbea3642f1a678e0af00026d5dc20575094
dalmo@osx <kristian@doingit.no>
2016-04-30 21:15:57 +0200

    Quick test for converting from/to labelled.

--- tests/testthat/test-labelled.R
+++ tests/testthat/test-labelled.R
@@ -1,7 +1,9 @@
 context("Labelled data")
 
+org <- haven::read_sav("sav.sav")
+
 test_that("Convert to labelled, i/o, and convert back" , {
-  sav <- haven::read_sav("sav.sav")
+  sav <- org
   fileName <- file.path(tempdir(), "sav.sav")
 
   # Create "mock" labelled variables and labels
@@ -24,4 +26,13 @@ test_that("Convert to labelled, i/o, and convert back" , {
 
 })
 
-# TODO
+test_that("Convert to_labelled without calling from_labelled first", {
+  res <- to_labelled(org)
+  expect_identical(as.character(lapply(res, attr, "label")), rep(NA_character_, 4))
+})
+
+test_that("data.table works with from/to_labelled", {
+  skip_if_not_installed("data.table")
+  res <- from_labelled(data.table::as.data.table(org))
+  res <- to_labelled(res)
+})
