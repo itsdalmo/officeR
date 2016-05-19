e4cc652ad5b752dadcbc5c85031bc650eb0a491e
dalmo@osx <kristian@doingit.no>
2016-05-19 23:28:53 +0200

    Changed description and added package documentation.

---
+++ R/officeR.R
@@ -0,0 +1,16 @@
+#' officeR: A toolbox for using R with office.
+#'
+#' This package is meant to provide a more seamless experience when using R with
+#' MS Office and "other programs". See links in details for more information.
+#'
+#' \itemize{
+#' \item Get data in and out of R with \code{\link{read_data}} and \code{\link{write_data}}.
+#' \item Access sharepoint using \code{\link{sharepoint_link}} and/or \code{\link{sharepoint_mount}}.
+#' \item Read/write to the clipboard on Windows and Os X using \code{\link{from_clipboard}} and \code{\link{to_clipboard}}.
+#' \item Send data or plots to Excel and Powerpoint with \code{\link{to_excel}} and \code{\link{to_ppt}}.
+#' }
+#' @author Kristian D. Olsen
+#' @docType package
+#' @name officeR
+
+NULL
